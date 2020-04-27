c5308f8cb617b354245b55d189f11b4b3a820a6a
FledgeXu <xzk0701@gmail.com>
2020-04-27 12:24:40 +0800

    add the food.

--- src/main/java/com/tutorial/neutrino/food/ObsidianApple.java
+++ src/main/java/com/tutorial/neutrino/food/ObsidianApple.java
@@ -2,6 +2,7 @@ package com.tutorial.neutrino.food;
 
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
@@ -14,6 +15,6 @@ public class ObsidianApple extends Item {
             .build();
 
     public ObsidianApple() {
-        super(new Properties().food(food));
+        super(new Properties().food(food).group(ItemGroup.FOOD));
     }
 }
