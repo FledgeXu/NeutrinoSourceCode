4d5437cb8d60e45f1e3c2abd77189e2259af7f9b
FledgeXu <xzk0701@gmail.com>
2020-04-28 21:27:28 +0800

    Update the Block shape.

--- src/main/java/com/tutorial/neutrino/nonesoildblock/ObsidianFrame.java
+++ src/main/java/com/tutorial/neutrino/nonesoildblock/ObsidianFrame.java
@@ -1,10 +1,33 @@
 package com.tutorial.neutrino.nonesoildblock;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 
 public class ObsidianFrame extends Block {
+    private static VoxelShape shape;
+
+    static {
+        VoxelShape base = Block.makeCuboidShape(0, 0, 0, 16, 1, 16);
+        VoxelShape column1 = Block.makeCuboidShape(0, 1, 0, 1, 15, 1);
+        VoxelShape column2 = Block.makeCuboidShape(15, 1, 0, 16, 15, 1);
+        VoxelShape column3 = Block.makeCuboidShape(0, 1, 15, 1, 15, 16);
+        VoxelShape column4 = Block.makeCuboidShape(15, 1, 15, 16, 15, 16);
+        VoxelShape top = Block.makeCuboidShape(0, 15, 0, 16, 16, 16);
+        shape = VoxelShapes.or(base, column1, column2, column3, column4, top);
+    }
+
     public ObsidianFrame() {
         super(Properties.create(Material.ROCK).hardnessAndResistance(5).notSolid());
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return shape;
     }
 }
