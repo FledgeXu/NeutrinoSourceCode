058ff25d7a560ba0194304a14eb4c1b03962416e
FledgeXu <xzk0701@gmail.com>
2020-05-15 10:07:35 +0800

    Typo fix.

--- src/main/java/com/tutorial/neutrino/configure/Config.java
+++ src/main/java/com/tutorial/neutrino/configure/Config.java
@@ -9,7 +9,7 @@ public class Config {
     static {
         ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
         COMMON_BUILDER.comment("General settings").push("general");
-        VALUE = COMMON_BUILDER.comment("Test config vaule").defineInRange("value", 10, 0, Integer.MAX_VALUE);
+        VALUE = COMMON_BUILDER.comment("Test config value").defineInRange("value", 10, 0, Integer.MAX_VALUE);
         COMMON_BUILDER.pop();
         COMMON_CONFIG = COMMON_BUILDER.build();
     }
