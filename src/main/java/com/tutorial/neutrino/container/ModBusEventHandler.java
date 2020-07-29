b672294eae465e2d3a8bc91a5018b1d5de6b98bc
FledgeXu <xzk0701@gmail.com>
2020-07-29 11:46:06 +0800

    Fix Typo.

--- src/main/java/com/tutorial/neutrino/container/ModBusEventHandler.java
+++ src/main/java/com/tutorial/neutrino/container/ModBusEventHandler.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModBusEventHandler {
     @SubscribeEvent
-    public static void onClineSetupEvent(FMLClientSetupEvent event) {
+    public static void onClientSetupEvent(FMLClientSetupEvent event) {
         ScreenManager.registerFactory(ContainerTypeRegistry.obsidianFirstContainer.get(), (ObsidianFirstContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) -> {
             return new ObsidianFirstContainerScreen(screenContainer, inv, titleIn);
         });
