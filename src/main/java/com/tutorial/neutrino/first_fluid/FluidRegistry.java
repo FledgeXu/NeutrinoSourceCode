70903982aacd0e202969996fd255efbfd582aa93
FledgeXu <xzk0701@gmail.com>
2020-08-14 21:22:10 +0800

    Update Networking.

--- src/main/java/com/tutorial/neutrino/first_fluid/FluidRegistry.java
+++ src/main/java/com/tutorial/neutrino/first_fluid/FluidRegistry.java
@@ -20,5 +20,5 @@ public class FluidRegistry {
     public static RegistryObject<FlowingFluid> obsidianFluidFlowing = FLUIDS.register("obsidian_fluid_flowing", () -> {
         return new ForgeFlowingFluid.Flowing(FluidRegistry.PROPERTIES);
     });
-    public static ForgeFlowingFluid.Properties PROPERTIES = new ForgeFlowingFluid.Properties(obsidianFluid, obsidianFluidFlowing, FluidAttributes.builder(STILL_OIL_TEXTURE, FLOWING_OIL_TEXTURE).color(0xFF311cbb).density(10)).bucket(ItemRegistry.obsidianFluidBucket).block(BlockRegistry.obsidianRubikCube).slopeFindDistance(3).explosionResistance(100F);
+    public static ForgeFlowingFluid.Properties PROPERTIES = new ForgeFlowingFluid.Properties(obsidianFluid, obsidianFluidFlowing, FluidAttributes.builder(STILL_OIL_TEXTURE, FLOWING_OIL_TEXTURE).color(0xFF311cbb).density(4000).viscosity(4000)).bucket(ItemRegistry.obsidianFluidBucket).block(BlockRegistry.obsidianRubikCube).slopeFindDistance(3).explosionResistance(100F);
 }
--- src/main/java/com/tutorial/neutrino/network/Networking.java
+++ src/main/java/com/tutorial/neutrino/network/Networking.java
@@ -6,6 +6,7 @@ import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public class Networking {
     public static SimpleChannel INSTANCE;
+    public static final String VERSION = "1.0";
     private static int ID = 0;
 
     public static int nextID() {
@@ -14,15 +15,15 @@ public class Networking {
 
     public static void registerMessage() {
         INSTANCE = NetworkRegistry.newSimpleChannel(
-                new ResourceLocation("neutrino" + ":first_networking"),
+                new ResourceLocation("neutrino", "first_networking"),
                 () -> {
-                    return "1.0";
+                    return VERSION;
                 },
-                (s) -> {
-                    return true;
+                (version) -> {
+                    return version.equals(VERSION);
                 },
-                (s) -> {
-                    return true;
+                (version) -> {
+                    return version.equals(VERSION);
                 });
         INSTANCE.registerMessage(
                 nextID(),
@@ -33,7 +34,7 @@ public class Networking {
                 (buffer) -> {
                     return new SendPack(buffer);
                 },
-                (pack,ctx) ->{
+                (pack, ctx) -> {
                     pack.handler(ctx);
                 }
         );
